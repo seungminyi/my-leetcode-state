f0358583c1f522315a43bddff2e768feb9abe00a
seungminyi <62599371+seungminyi@users.noreply.github.com>
2024-02-13 18:21:14 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (49.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,20 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int result = 0;
-        boolean isFirst = true;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == ' ') {
-                isFirst = true;
-            }else{
-                if(isFirst) {
-                    result = 1;
-                    isFirst = false;
-                }else{
-                    result++;
-                }
+        for(int i = s.length() -1; i >= 0; i--) {
+            if(s.charAt(i) == ' ' && result != 0) {
+                return result;
+            }
+            
+            if(s.charAt(i) != ' '){
+                result++;
             }
         }
-        
         return result;
     }
 }
