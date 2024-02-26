3bf506adcf4b59c627c2eea1c20ecddabfe49a7f
seungminyi <62599371+seungminyi@users.noreply.github.com>
2024-02-26 13:26:19 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (43.14%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,22 +1,22 @@
 class Solution {
     public int hIndex(int[] citations) {
-        citations = Arrays.stream(citations).boxed()
-                     .sorted(Collections.reverseOrder())
-                     .mapToInt(Integer::intValue)
-                     .toArray();
-        int result = 0;
-        int left = 0;
-        int right = citations.length -1;
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(citations[mid] >= mid+1) {
-                result = mid+1;
-                left = mid + 1;
+        int n = citations.length;
+        int[] buckets = new int[n+1];
+        for(int c : citations) {
+            if(c > n) {
+                buckets[n]++;
             }else{
-                right = mid -1;
+                buckets[c]++;
             }
         }
         
-        return result;
+        int count = 0;
+        for(int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if(count >= i) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
